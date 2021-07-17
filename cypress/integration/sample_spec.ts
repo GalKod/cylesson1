af15e62779ac8c11d3869a5d80dd37777c808775
GalKod <gkodola@gmail.com>
2021-07-17 17:16:31 +0300

    cyTest home work1

---
+++ cypress/integration/sample_spec.ts
@@ -0,0 +1,22 @@
+/// <reference types="cypress" />
+
+describe('My First Test', () => {
+    it('Visits the Kitchen Sink', () => {
+        cy.visit('https://ita-social-projects.github.io/GreenCityClient/#/')
+        cy.get('.header_sign-up-btn > span').click()
+        cy.get('.form-content-container > .primary-global-button').should('be.visible')
+        cy.get('#email')
+            .type('fake@email.com')
+            .should('have.value', 'fake@email.com')
+        cy.get('#firstName')
+            .type('fake.name')
+            .should('have.value', 'fake.name')
+        cy.get('#password')
+            .type('Fake_password_1')
+            .should('have.value', 'Fake_password_1')
+        cy.get('#repeatPassword')
+            .type('Fake_password_1')
+            .should('have.value', 'Fake_password_1')
+        cy.get('.form-content-container > .primary-global-button').should('not.be.disabled')
+    })
+})
